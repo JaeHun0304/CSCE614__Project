10637fd43bac47c1973acee7ac54e6cbf14d5aa6
JaeHun Jung <45073917+JaeHun0304@users.noreply.github.com>
2019-03-17 22:18:43 -0500

    Create TREETester.scala

---
+++ src/test/scala/tree/TREETester.scala
@@ -0,0 +1,29 @@
+
+package tree
+
+import java.io.File
+
+import chisel3.iotesters
+import chisel3.iotesters.{PeekPokeTester}
+
+class TREETester(c: TREE) extends PeekPokeTester(c){
+	poke(c.io.in, 180)
+	step(1)
+	expect(c.io.out, 4)
+
+	poke(c.io.in, 120)
+	step(1)
+	expect(c.io.out, 3)
+
+	poke(c.io.in, 90)
+	step(1)
+	expect(c.io.out, 2)
+
+	poke(c.io.in, 70)
+	step(2)
+	expect(c.io.out, 1)
+
+	poke(c.io.in, 40)
+	step(2)
+	expect(c.io.out, 0)
+}
