0ad6a1d2e9465cc64c5f6435c18b276ea7015ac9
JaeHun Jung <45073917+JaeHun0304@users.noreply.github.com>
2019-03-17 21:21:57 -0500

    Create TREE.scala

---
+++ src/main/scala/tree/TREE.scala
@@ -0,0 +1,32 @@
+
+package tree
+
+import chisel3._
+
+class TreeClassification extends Module {
+	val io = IO(new Bundle {
+		val in = Input(UInt(16.W))
+		val out = Output(UInt(16.W))
+	})
+
+	val regA = RegInit(UInt(16.W), 0.U)	//temporary results will be stored in registers
+	val regB = RegInit(UInt(16.W), 0.U)
+	val regC = RegInit(UInt(16.W), 0.U)
+
+	when(io.in < 100.U){regA := io.in}	//classify input based on the generated tree
+		.otherwise{regB := io.in}
+
+	when(regA < 80.U){regC := regA}
+		.otherwise{io.out := 2.U}
+
+	when(regB < 130.U){io.out := 3.U}
+		.otherwise{io.out := 4.U}
+
+	when(regC < 50.U){io.out := 0.U}
+		.otherwise{io.out := 1.U}
+
+}
+
+object TreeClassification extends App {
+  chisel3.Driver.execute(args, () => new TreeClassification)
+}
