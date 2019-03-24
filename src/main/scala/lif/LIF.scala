208f3352148445cad54dcd95670bf28c6f4ced61
JaeHun Jung <45073917+JaeHun0304@users.noreply.github.com>
2019-03-24 17:31:29 -0500

    modified:   src/main/scala/lif/LIF.scala

--- src/main/scala/lif/LIF.scala
+++ src/main/scala/lif/LIF.scala
@@ -18,16 +18,17 @@ class LIF extends Module {
     val reset = Input(Bool())
     val sj1, sj2, sj3 = Input(UInt(1.W))
     val firing_out = Output(Bool())
+    val vi = Output(UInt(5.w))
   })
 
   val sj1_prev, sj2_prev, sj3_prev = RegInit(0.U(1.W))  // sj1_prev, sj2_prev, and sj3_prev registers save 1-bit Sj[t-1]
-  val vi, vi_prev = RegInit(0.U(5.W)) // vi and vi_prev registers save membrane potential at t and at (t-1) respectively
+  val vi_prev = RegInit(0.U(5.W)) // vi and vi_prev registers save membrane potential at t and at (t-1) respectively
 
   io.firing_out := 0.U   //initialize firing output waveform to zero
 
 // When reset is treu(1), initialize registers, note that vi is initialized into resting poetntial which is 6
   when(io.reset === 1.U){  
-  	vi := 6.U
+  	io.vi := 6.U
   	vi_prev := vi      //Initialize Vi[t-1] with Vi[t] for t=0 case
   	sj1_prev := io.sj1 //For the first input, initialize Sj[t-1] with input of Sj[t] for t=0 case
   	sj2_prev := io.sj2
@@ -38,13 +39,13 @@ class LIF extends Module {
   	// When Vi[t] memebrane potential exceeds threshold, set firing_out flag and reset Vi[t] to resting potential.
   	when(vi >= 14.U){
   		io.firing_out := 1.U
-  		vi := 6.U
+  		io.vi := 6.U
   	}
     // After computation and comparison are finisehd, update Sj[t-1] and Vi[t-1] for the next calculation.
   	sj1_prev := io.sj1
   	sj2_prev := io.sj2
   	sj3_prev := io.sj3
-  	vi_prev := vi
+  	vi_prev := io.vi
   }
 
 }
